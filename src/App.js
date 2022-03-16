53706bb6cd76f1e1cb33cc5feb8e542a0520647d
Sudip Kundu <sudipkundu999@gmail.com>
2022-03-16 20:30:33 +0530

    feat : Homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import { Header, ThemeToggleButton } from "./components";
+import { Cart, Homepage, Login, Products, Signup, Wishlist } from "./pages";
+
 function App() {
-  return <h1>CobraStore</h1>;
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+
+      <ThemeToggleButton />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import "./component-css/header.css";
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="navigation-container">
+        <Link to="/">
+          <h2>CobraStore</h2>
+        </Link>
+        <input
+          type="text"
+          placeholder="Search"
+          className="nav-search search-desktop"
+        />
+        <div className="nav-right">
+          <a href="/">
+            <button class="btn btn-secondary">Logout</button>
+          </a>
+          <Link className="nav-links" to="/login">
+            <i className="fas fa-user fa-2x"></i>
+            <span>User</span>
+          </Link>
+          <Link className="nav-links" to="/wishlist">
+            <i className="fas fa-heart fa-2x"></i>
+            <span>Wishlist</span>
+          </Link>
+          <Link className="nav-links" to="/cart">
+            <i className="fas fa-shopping-bag fa-2x"></i>
+            <span>Bag</span>
+          </Link>
+        </div>
+      </div>
+      <input
+        type="text"
+        placeholder="Search"
+        className="nav-search search-mobile"
+      />
+    </header>
+  );
+};
