4110b73caf5dbf8871ec38440015733f2530569b
Sudip Kundu <sudipkundu999@gmail.com>
2022-03-25 17:37:20 +0530

    refactor : reduce toast display time

--- src/utils/useNotifyUser.jsx
+++ src/utils/useNotifyUser.jsx
@@ -5,9 +5,9 @@ import { toast } from "react-toastify";
  * @param {string} message Takes a message
  * @returns Alerts the user about the message without blocking the whole screen
  */
-const notifyDefault = (message) => toast(message);
-const notifySuccess = (message) => toast.success(message);
-const notifyError = (message) => toast.error(message);
-const notifyInfo = (message) => toast.info(message);
+const notifyDefault = (message) => toast(message, { autoClose: 3000 });
+const notifySuccess = (message) => toast.success(message, { autoClose: 3000 });
+const notifyError = (message) => toast.error(message, { autoClose: 3000 });
+const notifyInfo = (message) => toast.info(message, { autoClose: 3000 });
 
 export { notifyDefault, notifySuccess, notifyError, notifyInfo };
