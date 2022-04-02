831dd3c46cdf0ed8f93f400ce79842f63344dd80
Sudip Kundu <sudipkundu999@gmail.com>
2022-04-02 15:09:34 +0530

    style: toastify height & display time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./toastify.css";
 import { Header, ThemeToggleButton } from "./components";
 import { useTheme } from "./contexts";
 import {
