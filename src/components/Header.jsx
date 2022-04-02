ad079ba1323fbd31b63a341bd2fd0ecb4b5dd510
Sudip Kundu <sudipkundu999@gmail.com>
2022-04-02 14:00:32 +0530

    feat : badge - cart & wishlist

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
+import { useCart, useWishlist } from "../contexts";
 import { useAuth } from "../contexts/auth-context";
 import "./component-css/header.css";
 
 export const Header = () => {
   const { userName } = useAuth();
+
+  const { wishlistToShow } = useWishlist();
+  const wishlistCount = wishlistToShow.length;
+
+  const { cartToShow } = useCart();
+  const cartCount = cartToShow.reduce((acc, curr) => (acc = acc + curr.qty), 0);
 
   return (
     <header>
@@ -26,10 +33,16 @@ export const Header = () => {
           </Link>
           <Link className="nav-links" to="/wishlist">
             <i className="fas fa-heart fa-2x"></i>
+            {wishlistCount !== 0 && (
+              <div className="header-badge wishlist-badge">{wishlistCount}</div>
+            )}
             <span>Wishlist</span>
           </Link>
           <Link className="nav-links" to="/cart">
             <i className="fas fa-shopping-bag fa-2x"></i>
+            {cartCount !== 0 && (
+              <div className="header-badge cart-badge">{cartCount}</div>
+            )}
             <span>Bag</span>
           </Link>
         </div>
