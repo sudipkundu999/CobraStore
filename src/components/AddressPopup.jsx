faa2c7a6786204129ba9c70cca22a861140f72d3
Sudip Kundu <sudipkundu999@gmail.com>
2022-05-06 15:54:41 +0530

    refactor : imports

--- src/components/AddressPopup.jsx
+++ src/components/AddressPopup.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAddress } from "../contexts";
 import "./component-css/address-popup.css";
 
