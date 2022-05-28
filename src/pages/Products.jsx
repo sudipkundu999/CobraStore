18ab61b2d2ed798db490c994137f7cf610e4b8d9
Sudip Kundu <sudipkundu999@gmail.com>
2022-05-28 14:09:33 +0530

    feat : mobile filters

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ProductCard, ProductFilter } from "../components";
 import { useProducts } from "../contexts";
 import { useDocumentTitle } from "../utils";
@@ -6,11 +7,18 @@ import "./pages-css/products.css";
 export const Products = () => {
   useDocumentTitle("Products");
   const { loadingProducts, productsToShow } = useProducts();
-
+  const [isFilterVisible, SetIsFilterVisible] = useState(false);
+  const toggleSidebarVisible = () => SetIsFilterVisible((x) => !x);
   return (
     <div className="products-body">
-      <ProductFilter />
-
+      <i
+        class="fas fa-bars fa-3x filter-mobile-btn"
+        onClick={() => toggleSidebarVisible()}
+      />
+      <ProductFilter
+        toggleSidebarVisible={toggleSidebarVisible}
+        isFilterVisible={isFilterVisible}
+      />
       <main className="products-main">
         {loadingProducts ? (
           <div className="loading-screen">Loading Products...</div>
